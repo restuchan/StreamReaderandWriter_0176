8651361e1a7ca9f93b003e40da7388fcb616e556
restuchan <restu.chan188@gmail.com>
2025-06-09 15:28:01 +0700

    menambahkan try

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -2,4 +2,9 @@
 using namespace std;
 
 int main() {
+   try{
+        cout << "Selamat datang di Prodi TI UMY" << endl;
+        throw 3.5; //melemparkan sebuah integer maka:
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
     
