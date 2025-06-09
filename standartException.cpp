74fc20568b8e498173b393b66c78288d3b1c5144
restuchan <restu.chan188@gmail.com>
2025-06-09 15:42:45 +0700

    catch

--- standartException.cpp
+++ standartException.cpp
@@ -14,4 +14,13 @@ int main()
         cout << data.at(5)<< endl;
         //memanggil array elemen ke 5
     }
-    
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+
+    return 0;
+};
